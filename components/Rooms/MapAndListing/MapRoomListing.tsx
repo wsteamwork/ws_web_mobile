9c973185b4b29bae2a99b2a8575b965c1ee4fadb
Duc Nhat <phamducnhat96bkhn@gmail.com>
2019-12-02 09:59:54 +0700

    done

--- components/Rooms/MapAndListing/MapRoomListing.tsx
+++ components/Rooms/MapAndListing/MapRoomListing.tsx
@@ -50,7 +50,6 @@ const MapRoomListing: FC = (props) => {
     lazyLoad: 'ondemand',
     swipeToSlide: true,
     touchThreshold: 100,
-    beforeChange: (current, next) => alert(next),
     responsive: [
       {
         breakpoint: 768,
